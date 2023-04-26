d858e713e4a191f01e572cb9b5ad42c59fb226e8
ophaant <vilfanvenci@gmail.com>
2023-04-26 17:07:01 +0700

    add whitelistip or auth:api middleware statis

---
+++ app/Http/Middleware/IpWhiteListMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class IpWhiteListMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $ipWhitelist = ['127.0.0.1']; // Replace with your own whitelist
+        $clientIp = $request->getClientIp();
+
+        if (in_array($clientIp, $ipWhitelist)) {
+            // If the client IP is in the whitelist, allow access
+            return $next($request);
+        }
+
+        // Otherwise, check if the user is authenticated via the `auth:api` middleware
+        if (auth()->guard('api')->check()) {
+            return $next($request);
+        }
+
+       throw new UnauthorizedHttpException('Unauthorized', 'Unauthorized');
+    }
+}
